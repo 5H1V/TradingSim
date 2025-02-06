21f50b2b487a0627c27a83f9bb0dd493450fb6e1
5H1V <67129520+5H1V@users.noreply.github.com>
2025-02-06 00:11:32 -0500

    creating pages and urls

--- djangotutorial/tradingapp/views.py
+++ djangotutorial/tradingapp/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def index(request):
+    return HttpResponse("Hello, index.")
+
+def login(request):
+    return HttpResponse("Hello, login.")
+
+def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
