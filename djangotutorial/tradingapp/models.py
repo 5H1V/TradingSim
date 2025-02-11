a97265c847066d5600392e31bdfeed2d9030b1f6
5H1V <spatel0240@gmail.com>
2025-02-11 17:25:59 -0500

    edited admin page

--- djangotutorial/tradingapp/models.py
+++ djangotutorial/tradingapp/models.py
@@ -1,6 +1,5 @@
-import datetime
 from django.db import models
-from django.utils import timezone
+
 # Stock
 # Time data 
 # Price
@@ -8,21 +7,6 @@ from django.utils import timezone
 # Transaction ID
 # User ID
     
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-    def __str__(self):
-        return self.question_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
-
-    
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
