f011e152e7909b50f9f0d889b6e37f6c16ce8866
5H1V <spatel0240@gmail.com>
2025-02-13 23:59:13 -0500

    Improved structure

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add out new application
+    'tradingapp',
 ]
 
 MIDDLEWARE = [
