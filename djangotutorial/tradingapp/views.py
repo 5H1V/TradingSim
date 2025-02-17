ff801d3a8be905d4d2b75fcc126fb28bdd2bee4c
5H1V <spatel0240@gmail.com>
2025-02-17 16:27:20 -0500

    inserted temporary html layouts for login

--- djangotutorial/tradingapp/views.py
+++ djangotutorial/tradingapp/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, HttpResponse
 from .models import TodoItem
 
 def login(request):
-    return render(request, "main/login.html")
+    return render(request, "login.html")
 
 def home(request):
-    return render(request, "main/home.html")
+    return render(request, "home.html")
 
 def wallet(request):
-    return render(request, "main/wallet.html")
+    return render(request, "wallet.html")
