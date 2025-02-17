a0c078a80ea388458ecaf690af6e551f5a4ad20f
5H1V <spatel0240@gmail.com>
2025-02-17 14:24:35 -0500

    added model psuedocode

--- djangotutorial/tradingapp/models.py
+++ djangotutorial/tradingapp/models.py
@@ -1,12 +1,34 @@
 from django.db import models
-
-# Stock
-# Time data 
-# Price
-# Quantity Bought
-# Transaction ID
-# User ID
     
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+class User(models.Model):
+    user_name
+    user_ID
+    password
+    trading_wallet
+    stocks_owned
+
+class trading_wallet(models.Model):
+    stocks_owned
+    password
+    
+class stocks_owned(models.Model):
+    stock
+    time data 
+    price
+    quantity Bought
+    transaction ID
+    user_ID
+
+class stocks(models.Model):
+    price
+    name
+    ticker
+    history
+    
+class History(models.Model):
+    dates
+    prices
